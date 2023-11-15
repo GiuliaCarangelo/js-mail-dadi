42ced52789a033a9e18227d4a489707e9b6ce6d4
GiuliaCarangelo <tnhkagura@gmail.com>
2023-11-16 00:15:41 +0100

    add guide comments to js

---
+++ mail/js/script.js
@@ -0,0 +1,8 @@
+// Chiedi all’utente la sua email
+prompt("Inserisci la tua e-mail");
+
+// createvi una piccola lista di email (array)
+
+// controlla che sia nella lista di chi può accedere (for)
+
+// stampa un messaggio appropriato sull’esito del controllo.
